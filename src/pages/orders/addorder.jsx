c5c83684385b9b9a611839d55b518c862327ca1f
Pratik Bomzan <121595947+hckkakashi@users.noreply.github.com>
2025-02-16 16:49:47 +0545

    Created Add Order Page

---
+++ src/pages/orders/addorder.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dashboard from "../../components/dashboard/Dashboard";
+const AddOrders = () => {
+  return (
+       <>
+         <Dashboard mainContent={
+           <div className='Container'>
+            <h1>Add Orders</h1>
+            
+           </div>
+         } />
+       </>
+  )
+}
+
+export default AddOrders
