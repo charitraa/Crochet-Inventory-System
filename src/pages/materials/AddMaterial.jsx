1d00735390314eba8ab91f856ef9b536f5c22c0c
Pratik Bomzan <121595947+hckkakashi@users.noreply.github.com>
2025-02-16 14:09:14 +0545

    Created Add Material Page

--- src/pages/materials/AddMaterial.jsx
+++ src/pages/materials/AddMaterial.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Dashboard from "../../components/dashboard/Dashboard";
+import "./AddMaterial.css"
 
 const AddMaterials = () => {
   return (
     <>
       <Dashboard mainContent={
-        <div className=' text-2xl text-red-700'>
+        <div className='Container'>
          <h1>Add Material</h1>
          <form>
             <label htmlFor="materialname">Material Name</label>
@@ -13,6 +14,29 @@ const AddMaterials = () => {
 
             <label htmlFor="colour">Colour</label>
             <input type="text" placeholder='colour' />
+
+            <label htmlFor="category">Category</label>
+            <select id="category" className="p-2 border rounded-lg w-64">
+  <option value="">Select a category</option>
+  <option value="electronics">Wool</option>
+  <option value="fashion">Beads</option>
+  <option value="books">Wrapping Paper</option>
+  <option value="home-appliances">Ribbon</option>
+</select>
+
+            <label htmlFor="stock">Stock Quantity</label>
+            <input type="text" placeholder='Stock' />
+
+            <label htmlFor="price">Price</label>
+            <input type="text" placeholder='Price' />
+
+            <label htmlFor="image">Image</label>
+            <input type="file" placeholder='Upload Image' name='image'/>
+
+            <button type='button'>ADD</button>
+            <button type='button'>CANCEL</button>
+
+                
          </form>
         </div>
       } />
