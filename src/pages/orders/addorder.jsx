103581259775e3ab2ee3e64c142a47dc672a7224
Pratik Bomzan <121595947+hckkakashi@users.noreply.github.com>
2025-02-18 07:50:31 +0545

    Creating Add Order Page

--- src/pages/orders/addorder.jsx
+++ src/pages/orders/addorder.jsx
@@ -6,6 +6,44 @@ const AddOrders = () => {
          <Dashboard mainContent={
            <div className='Container'>
             <h1>Add Orders</h1>
+            <form>
+            <label htmlFor="customernameroductname">Customer Name</label>
+            <input type="text" placeholder='Name' />
+
+            <label htmlFor="orderitem">Order Item</label>
+            <input type="text" placeholder='Name' />
+
+            <label htmlFor="orderdate">Order Date</label>
+            
+
+            <label htmlFor="quantity">Quantity</label>
+            <input type="text" placeholder='Quantity' />
+
+            <label htmlFor="Price">Price</label>
+            <input type="text" placeholder='Price' />
+
+            <label htmlFor="status">Status</label>
+            <select id="status" className="p-2 border rounded-lg w-64">
+            <option value="">Select a category</option>
+            <option value="pending">Pending</option>
+            <option value="placed">Placed</option>
+            <option value="completed">Completed</option>
+
+           </select>
+           <label htmlFor="customization">Customization</label>
+           <input type="text" placeholder='Describe' />
+
+           <button type='button'>ADD</button>
+            <button type='button'>CANCEL</button>
+
+
+
+
+
+
+          
+             </form>
+
             
            </div>
          } />
